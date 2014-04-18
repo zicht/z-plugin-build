c479b905480185effd9791eee7fcbbe7737bb9a0
Gerard van Helden <gerard@zicht.nl>
2014-04-18 13:47:09 +0000

    Copying svn://zon/projecten/zicht-libraries/z-plugins/trunk/build to svn://zon/projecten/zicht-libraries/z-plugin-build/trunk/build
    git-svn-id: svn://zon/projecten/zicht-libraries/z-plugin-build/trunk@35667 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ build/Plugin.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Gerard van Helden <gerard@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+namespace Zicht\Tool\Plugin\Build;
+
+use \Zicht\Tool\Plugin as BasePlugin;
+use \Zicht\Tool\Container\ContainerBuilder;
+use \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use \Zicht\Tool\Container\Container;
+
+class Plugin extends BasePlugin
+{
+    public function appendConfiguration(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode
+            ->children()
+                ->arrayNode('build')
+                    ->children()
+                        ->scalarNode('dir')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+}
